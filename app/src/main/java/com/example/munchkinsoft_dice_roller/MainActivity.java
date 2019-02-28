60dcdc216233fdef495bd6021cd1e9e1c3e7a9d4
Epic Treestick <cello.mic.go@gmail.com>
2019-02-27 23:08:53 -0600

    project files

---
+++ app/src/main/java/com/example/munchkinsoft_dice_roller/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.munchkinsoft_dice_roller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
